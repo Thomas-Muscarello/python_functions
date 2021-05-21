0e11d96e3e399c6f521a7b5031b2022cbca165c1
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-05-20 20:39:11 -0400

    function with parameters

---
+++ functionWithParams.py
@@ -0,0 +1,6 @@
+#You can also give a fucntion a parameter adding the param in the function name and in the function itself:
+def sayhi(name):
+    print("Hello "+ name)
+
+# To call a function enter a parameter into the function"
+sayhi("Thomas")
