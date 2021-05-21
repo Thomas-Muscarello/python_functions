e22122ae7ef377136b49c8ea02aba7c03e6aba37
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-05-20 20:35:45 -0400

    creating my first function

---
+++ functions.py
@@ -0,0 +1,12 @@
+#Notes: Functions in Python are a collection of code to complete a task
+# First begin with a key word, than give the function a name, followed by "():"
+#Must make sure to indent the next line to stay in the function.
+#Example 1:
+
+def sayhi():
+    print("Hello User")
+
+# To call a function, call it's name followed by "()"
+sayhi()
+
+
